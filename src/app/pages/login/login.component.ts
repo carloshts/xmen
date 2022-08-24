9ad9a75a92c770d92b9738ec9b4c6acdcdde42fa
p626582 <carlos.hnunes@castgroup.com.br>
2022-08-24 16:01:54 -0300

    Aula 2 X men - Form Group e Validações

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public nome:FormControl = new FormControl({
+    value: 'PROF X',
+    disabled:false
+  },[Validators.required]);
+  public loginFormGroup:FormGroup = this.formBuilder.group(
+    {
+      nome: new FormControl({
+        value: 'PROF X',
+        disabled:false
+      },[Validators.required]),
+      senha:new FormControl('',[Validators.required,Validators.minLength(3)])
+    });
 
-  constructor() { }
+  constructor(
+    private formBuilder:FormBuilder,
+    private router:Router
+    ) { }
 
   ngOnInit(): void {
+  }
+  entrar(){
+    console.log(this.loginFormGroup['controls']['senha'])
+    //Validar os campos de login e seguir para Cerebro
+    if(this.loginFormGroup.valid){
+      console.log('Segue para Cerebro')
+      this.router.navigateByUrl('/cerebro')
+    }else{
+      console.log('Campos invalidos')
+      this.loginFormGroup.markAllAsTouched();
+    }
   }
 
 }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,8 +1,13 @@
+import { MaterialModule } from 'src/app/material/material.module';
 import { Route, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
 const routes:Route[] = [
   {
@@ -16,7 +21,14 @@ const routes:Route[] = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MaterialModule,
+    MatCardModule,
+    MatGridListModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
